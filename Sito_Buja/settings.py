d6fed9a2a30bdb90f3f4ad328d9e98ab8f7696a6
gino <gino.sartor@yahoo.it>
2024-01-25 18:46:25 +0100

    initial commi

--- Sito_Buja/settings.py
+++ Sito_Buja/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,8 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'Sito_Buja/Sito_Anziani/static/'
+STATIC_URL = '/static/'
 
+STATIC_ROOT = "/home/ginosartor/Sito_Buja/Sito_Anziani/static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
