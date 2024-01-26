ff2d5e991837836db1a5b4251474496b9fd6597a
gino <gino.sartor@yahoo.it>
2024-01-26 10:50:12 +0100

    initial commi

--- Sito_Buja/settings.py
+++ Sito_Buja/settings.py
@@ -129,8 +129,6 @@ STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
